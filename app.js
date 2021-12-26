3e2734e46a9f2439d0fdbb454faa2738480d2ffb
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2021-12-26 17:29:43 +0900

    object

---
+++ app.js
@@ -0,0 +1,16 @@
+const player = {
+    name: "kcs",
+    points: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.fat);
+
+player.fat = false;
+
+console.log(player.fat);
+player.points = player.points + 15
+player.lastName = "potato";
+console.log(player);
+
