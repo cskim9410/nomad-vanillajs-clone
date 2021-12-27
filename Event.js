8d0e8e5e88aaa402aa6d4d3a6571117f6a6afb53
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2021-12-27 16:15:06 +0900

    더많은 Events 추가 window. 기본제공하는 기능

--- Event.js
+++ Event.js
@@ -1,26 +1,49 @@
-const title = document.querySelector("div.hello h1");
+const h1 = document.querySelector("div.hello h1");
 
-console.dir(title);
+console.dir(h1);
 
-title.style.color = "blue";
+h1.style.color = "blue";
 
 
 function handleTitleClick() {
     const rancolor = ["blue", "red", "green", "yellow", "black", "brown"];
 
     let num = Math.floor(Math.random()*rancolor.length);
-    title.style.color = rancolor[num];
+    h1.style.color = rancolor[num];
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here"
+    h1.innerText = "Mouse is here"
 }
 
 function handleMouseLeave() {
-    title.innerText = "Mouse is gone"
+    h1.innerText = "Mouse is gone"
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+// document.body, title, head 는 중요한 부분이라 가능 나머지 Element는
+// querySelector나 getElementById로 가져와야한다.
 
+function handleWindowCopy() {
+    alert("copier!")
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI")
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOD")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleResize);
+window.addEventListener("copy", handleWindowCopy);
+
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
