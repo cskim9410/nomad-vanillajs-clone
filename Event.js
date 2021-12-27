01dfb69c2fd1c125dfbca2708bfeb3edb8545ec6
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2021-12-27 15:53:54 +0900

    console.dir로 사용할 수 있는 이벤트 확인, mouseenter, mouseleave 이용

--- Event.js
+++ Event.js
@@ -4,8 +4,23 @@ console.dir(title);
 
 title.style.color = "blue";
 
+
 function handleTitleClick() {
-    title.style.color = "Green";
+    const rancolor = ["blue", "red", "green", "yellow", "black", "brown"];
+
+    let num = Math.floor(Math.random()*rancolor.length);
+    title.style.color = rancolor[num];
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here"
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone"
 }
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
